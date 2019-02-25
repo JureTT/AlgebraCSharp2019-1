301a47ab318f3fb66f4d1cda099f898906789cb7
JureTT <jureperic@gmail.com>
2019-02-25 16:50:05 +0100

    uvjetno if ugnježdeni

---
+++ ConsoleApp1/primjer_5.2.1.2_pozitivan/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primjer_5._2._1._2_pozitivan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Unesite cijeli broj:");
+            int a = int.Parse(Console.ReadLine());
+            if ( a > 0 )
+            {
+                Console.WriteLine("Uneseni broj je pozitivan.");
+            }
+            else if ( a > 0 )
+            {
+                Console.WriteLine("Uneseni broj NIJE pozitivan.");
+            }
+            else
+            {
+                Console.WriteLine("Uneseni broj je jednak nuli.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
