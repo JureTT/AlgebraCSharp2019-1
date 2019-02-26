4c0b3d840ed384731366cb31a80a0e68b30ae4be
programer10 <programer10@ucione.local>
2019-02-26 16:17:32 +0100

    zadaci iz zbirke

---
+++ ConsoleApp1/zadatak 5.2.3 zbrika/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak_5._2._3_zbrika
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesite cijeli broj:");
+            int a = int.Parse(Console.ReadLine());
+
+            if (a >= 100)
+            {
+                Console.WriteLine("Uneseni broj je velik.");
+            }
+            else
+            {
+                Console.WriteLine("Uneseni broj NIJE velik.");
+            }
+            Console.ReadLine();
+        }
+    }
+}
