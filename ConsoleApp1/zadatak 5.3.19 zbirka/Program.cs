5310abf3215a66f7e64ffd3c596ba05f50ad204d
JureTT <jureperic@gmail.com>
2019-02-27 18:53:28 +0100

    zadatak 5.3.19 zbirka

---
+++ ConsoleApp1/zadatak 5.3.19 zbirka/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak_5._3._19_zbirka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program za zbrajanje brojeva.");
+
+            int suma = 0;
+            int a = -10000;
+            while (a != 0)
+            {
+                Console.WriteLine("Unesi jedan broj:");
+                a = int.Parse(Console.ReadLine());
+                suma += a; 
+            }
+            Console.Write("{0} ", suma);
+            
+            Console.ReadLine();
+        }
+    }
+}
