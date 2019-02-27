f45fef66d76014cb9d5cb2179d7fa409c438f285
JureTT <jureperic@gmail.com>
2019-02-27 18:40:58 +0100

    zadatak 5.3.17 zbirka

---
+++ ConsoleApp1/zadatak 5.3.15 zbirka/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak_5._3._15_zbirka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program ispisuje brojeve unatrag iz intervala [1, 10].");
+
+            for (int i = 10; i >= 1; i--)
+            {
+                Console.Write("{0} ", i);
+
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ConsoleApp1/zadatak 5.3.17 zbirka/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak_5._3._17_zbirka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // u zbirci je drugacije riješeno pogledati zbirku
+            Console.WriteLine("Ispis prvih 10 potencija broja 2.");
+
+            int potencija = 1;
+            for (int i = 1; i <= 10; i++)
+            {
+                potencija *= 2;
+                Console.Write("{0} ", potencija);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
