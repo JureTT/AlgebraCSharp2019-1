b3a9d412ec8caae0e24ba2461f6ed8490061d3a6
JureTT <jureperic@gmail.com>
2019-02-27 17:27:26 +0100

    zadatak 5.3.7 zbirka

---
+++ ConsoleApp1/zadatak 5.3.7 zbirka/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak_5._3._7_zbirka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ispis brojeva koji su djeljivi sa 7 i nedjeljivi sa 3 u intervalu [1, 100].");
+            
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 7 == 0 && i % 3 != 0)
+                {
+                    Console.Write("{0} ", i);
+                }
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
