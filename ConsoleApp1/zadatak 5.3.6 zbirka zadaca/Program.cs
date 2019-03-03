97f80dfacfccc87b372b022f8d9d45c6fec74b1d
Jure <jureperic@gmail.com>
2019-03-03 16:54:25 +0100

    zadaca 5.3.6

---
+++ ConsoleApp1/zadatak 5.3.6 zbirka zadaca/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak_5._3._6_zbirka_zadaca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program ispisuje sve prijestupne godine od 1900. do 2007.");
+
+            for (int i = 1500; i < 2019; i++)
+            {
+                if (i % 4 == 0 && (!(i % 100 == 0) || (i % 400 == 0)))
+                {
+                    Console.Write("{0}  ", i.ToString());
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
