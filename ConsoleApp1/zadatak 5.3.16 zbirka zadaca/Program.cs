01f5427c2611429c3edf5b050a2ab1846cfdd33b
Jure <jureperic@gmail.com>
2019-03-03 22:55:04 +0100

    zadaca 5.3.16 i 5.3.18

---
+++ ConsoleApp1/zadatak 5.3.16 zbirka zadaca/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak_5._3._16_zbirka_zadaca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program koji ispisuje potencija broja 2 manje od 1000.");
+
+            int a = 1;
+            for (int i = 0; i < 1000; i++)
+            {
+                if (a > 1000)
+                {
+                    break;
+                }
+                Console.WriteLine("{0}. potencija broja 2 je :  {1}", i, a);
+                a = a * 2;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/zadatak 5.3.18 zbirka zadaca/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak_5._3._18_zbirka_zadaca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program za provjeru unosa ocjene u intervalu [1, 5].");
+
+            Console.WriteLine("Unesite dobivenu ocjenu: ");
+            int a = int.Parse(Console.ReadLine()); ;
+            while(a < 1 || a > 5)
+            {
+                Console.WriteLine("Ocjena nije unutar zadanog intervala, pokušajte ponovo.");
+                a = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Ocjena zadovoljava zadane kriterije.");
+
+            Console.ReadKey();
+        }
+    }
+}
