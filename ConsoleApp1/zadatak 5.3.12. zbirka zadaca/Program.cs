8c1ae33e7af8d3dceb9128270d228ddf9ed3d6e2
Jure <jureperic@gmail.com>
2019-03-03 21:41:09 +0100

    zadaća 5.3.12 i 5.3.14

---
+++ ConsoleApp1/zadatak 5.3.12. zbirka zadaca/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak_5._3._12.zbirka_zadaca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program ispisuje zbroj i prosjek 5 unešenih brojeva.");
+
+            int zbroj = 0;
+            int prosjek = 0;
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Unesite {0}. prirodan broj:", i);
+                int a = int.Parse(Console.ReadLine());
+                zbroj += a;
+            }
+
+            prosjek = zbroj / 5;
+
+            Console.WriteLine("Zbroj svih unešenih brojeva je {0}, a njihov prosjek je {1}.", zbroj, prosjek);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/zadatak 5.3.14 zbirka zadaca/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak_5._3._14_zbirka_zadaca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program za ispis neparnih brojeva u intervalu [1, 10].");
+
+            for (int i = 1; i <= 10; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.WriteLine("{0}   ", i);
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
