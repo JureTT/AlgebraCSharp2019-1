cae247c2bdcd1d266312128e54839376faa10d75
Jure <jureperic@gmail.com>
2019-03-04 00:01:01 +0100

    zadaca 5.3.20 i 5.3.22

---
+++ ConsoleApp1/zadatak 5.3.20 zbirka zadaca/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak_5._3._20_zbirka_zadaca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program za brojanje parnih i neparnih brojeva");
+
+            Console.WriteLine("Unesite jedan prirodan broj:");
+            int a = int.Parse(Console.ReadLine());
+
+            int parni = 0;
+            int neparni = 0;
+            while (a != 0)
+            {
+                if (a % 2 == 0)
+                {
+                    parni++;
+                }
+                else 
+                {
+                    neparni++;
+                }
+                Console.WriteLine("Unesite jedan prirodan broj:");
+                a = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("Unešeno je {0} parnih i {1} neparnih brojeva.", parni, neparni);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/zadatak 5.3.22 zbirka zadaca/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak_5._3._22_zbirka_zadaca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program za izvršavanje osnovnih računski operacija.");
+            float a = 0, b = 0;
+            string operacija = "";
+            string odluka = "da";
+
+            while (odluka == "da")
+            {
+                Console.WriteLine("Unesi 1. prirodan broj:");
+                a = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Unesi 2. prirodan broj:");
+                b = float.Parse(Console.ReadLine());
+
+                Console.WriteLine("Unesi željenu operaciju ( + - * / ):");
+                operacija = Console.ReadLine();
+
+                switch (operacija)
+                {
+                    case "+":
+                    case "plus":
+                        Console.WriteLine("Zbroj je: {0} + {1} = {2}", a, b, (a + b));
+                        break;
+                    case "-":
+                        Console.WriteLine("Razlika je: {0} - {1} = {2}", a, b, (a - b));
+                        break;
+                    case "*":
+                    case "x":
+                        Console.WriteLine("Umnožak je: {0} * {1} = {2}", a, b, (a * b));
+                        break;
+                    case "/":
+                        Console.WriteLine("Kvocijent je: {0} / {1} = {2}", a, b, (a / b));
+                        break;
+                    default:
+                        Console.WriteLine("Nisi unio valjani znak za neku od mogućih operacija");
+                        break;
+                }
+
+                Console.WriteLine("Želite li računati ponovo (da/ne)?");
+                odluka = Console.ReadLine();
+
+                while (odluka != "da" && odluka != "ne")
+                {
+                    if (odluka == "d" || odluka == "D" || odluka == "DA" || odluka == "Da" || odluka == "dA")
+                    {
+                        odluka = "da";
+                    }
+                    else if (odluka == "n" || odluka == "N" || odluka == "NE" || odluka == "Ne" || odluka == "nE")
+                    {
+                        odluka = "ne";
+                    }
+                    else
+                    {
+                        Console.WriteLine("Neispravan unos pokušajte ponovo:");
+                        odluka = Console.ReadLine();
+                    }
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
+
