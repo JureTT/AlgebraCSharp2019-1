f8651e92b35711cb300c77df6663482987844dec
Jure <jureperic@gmail.com>
2019-03-03 20:56:35 +0100

    zadaca 5.3.8 i 5.3.10

---
+++ ConsoleApp1/zadatak 5.3.10 zbirka zadaca/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak_5._3._10_zbirka_zadaca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program za provjeru savršenih brojeva.");
+
+            Console.WriteLine("Unesi jedan broj:");
+            int a = int.Parse(Console.ReadLine());
+
+            int savrs = 0;
+            for (int i = 1; i < a; i++)
+            {
+                if(a % i == 0)
+                {
+                    savrs += i;
+                }
+            }
+
+            if (savrs == a)
+            {
+                Console.WriteLine("Zbroj svih djelitelja ovoga broja je {0}, prema tome broj je savršen.", savrs);
+            }
+            else
+            {
+                Console.WriteLine("Zbroj svih djelitelja ovoga broja je {0}, prema tome broj nije savršen.", savrs);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp1/zadatak 5.3.8 zbirka zadaca/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak_5._3._8_zbirka_zadaca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program koji ispisuje koliko ima brojeva djeljivih sa 7 i nedjeljivih sa 3 u intevalu [1, 1000].");
+
+            int count = 0;
+            for (int i = 1; i < 1000; i++)
+            {
+                if (i % 7 == 0 && i % 3 != 0)
+                {
+                    count++;
+                    //Console.Write("{0}  ", i);
+                }
+            }
+
+            Console.WriteLine("\nU intervalu od [1, 1000] ima {0} brojeva djeljivih sa 7 i nedjeljivih sa 3.", count);
+            Console.ReadKey();
+        }
+    }
+}
