dd391b640e6627921f5c81099fed617399d5b834
Jure <jureperic@gmail.com>
2019-03-05 21:57:03 +0100

    zadaca 7.2.1 i 7.2.2

---
+++ ConsoleApp1/zadatak 7.2.1 zbirka zadaca/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak_7._2._1_zbirka_zadaca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pogram za računanje opsega i površine kruga.");
+
+            Console.WriteLine("Unesite vrijednost polumjera kruga (cm):");
+            float polumjer = float.Parse(Console.ReadLine());
+
+            float opseg = Opseg(polumjer);
+            float povrsina = Povrsina(polumjer);
+            Console.WriteLine("Za unesenu vrijedonst polumjera kruga izračunali smo" +
+                " da je njegova površina {0} cm2; a njegov opseg {1} cm.", povrsina, opseg);
+
+            Console.ReadKey();
+        }
+        static float Opseg(float radijus)
+        {
+            float opseg = 2 * radijus * (float)Math.PI;
+            return opseg;
+        }
+        static float Povrsina(float radijus)
+        {
+            float povrsina = radijus * radijus * (float)Math.PI;
+            return povrsina;
+        }
+    }
+}
---
+++ ConsoleApp1/zadatak 7.2.2 zbirka zadaca/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace zadatak_7._2._2_zbirka_zadaca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pogram za računanje površine jednakokračnog trokuta.");
+
+            Console.WriteLine("Unesite duljinu osnovice trokuta (cm):");
+            float osnovica = float.Parse(Console.ReadLine());
+
+            Console.WriteLine("Unesite duljinu kraka trokuta (cm):");
+            float krak = float.Parse(Console.ReadLine());
+
+            float povrsina = Povrsina(osnovica, krak);
+            Console.WriteLine("Za unesene vrijedonsti duljina stranica trokuta izračunali smo da je njegova površina {0} cm2.", povrsina);
+
+            Console.ReadKey();
+        }
+        static float Povrsina(float osnovica, float krak)
+        {
+            float povrsina = (osnovica * krak) / 2;
+            return povrsina;
+        }
+    }
+}
