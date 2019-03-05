ca3892478ea37d7967d0e16ac7a3eaedaf890fa5
JureTT <jureperic@gmail.com>
2019-03-05 18:49:00 +0100

    procedure(metode)

---
+++ ConsoleApp1/primjer 7.2.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primjer_7._2._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pozdrav("Pero");
+            Pozdrav("Pero","Perić");
+            Pozdrav(prezime:"Perić", ime:"Ime");    // ako se želi drugim redosljedom unjeti varijable,
+                                                    // moraju se imenovati varijable i dodati im se vrijednosti dvotočkom 
+
+            Console.ReadLine();
+        }
+
+        static void Pozdrav(string ime)
+        {
+            Console.WriteLine("Dobar dan, " + ime + "!");
+        }
+
+        static void Pozdrav(string ime, string prezime)
+        {
+            Console.WriteLine("Dobar dan, " + ime + " " + prezime + "!");
+        }
+    }
+}
