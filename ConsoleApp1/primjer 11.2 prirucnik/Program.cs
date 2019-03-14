c48df17534396294c8f171029c1c953a7ba7cdac
JureTT <jureperic@gmail.com>
2019-03-14 18:14:53 +0100

    linq

---
+++ ConsoleApp1/primjer 11.2 prirucnik/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BuisnessLayer;
+
+namespace primjer_11._2_prirucnik
+{
+    class Program
+    {
+        static void Main(string[] args) 
+        {
+            Ucenik uc1 = new Ucenik("Maja", "Majić");
+            List<Osoba> osobe = new List<Osoba>();
+        }
+    }
+}
