fac81a9bed57b7d1253c1c7a20496870ba917a26
JureTT <jureperic@gmail.com>
2019-03-15 18:33:30 +0100

    promjene

---
+++ ConsoleApp1/3. zadatak/Automobil.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3.zadatak
+{
+    class Automobil
+    {
+        private string naziv;
+        private int godinaPRoizvodnje;
+        private double osnovnaCijena;
+
+        public string Naziv { get => naziv; set => naziv = value; }
+        public int GodinaPRoizvodnje { get => godinaPRoizvodnje; set => godinaPRoizvodnje = value; }
+        public double OsnovnaCijena { get => osnovnaCijena; set => osnovnaCijena = value; }
+
+        public int Starost()
+        {
+            int godine = DateTime.Now.Year - GodinaPRoizvodnje;
+            return godine;
+        }
+    }
+}
